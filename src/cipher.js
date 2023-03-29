56964c6c2173d3207c529571cfff84c4f8e5885f
ceciliapantae <ceciliapantae93@gmail.com>
2023-03-29 17:12:30 -0500

    Agregue el TypeError y solucione lo del decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
   encode: function(offset_value1,cipherMessage){
+    if( typeof cipherMessage !== "string"){
+      throw new TypeError("The argument must be a string");
+    } 
     let result = '';
     for (let index = 0; index < cipherMessage.length; index++) {
       
@@ -9,14 +12,22 @@ const cipher = {
     } return result; 
   },
   decode: function(offsetValue2,decipherMessage){
+    if( typeof decipherMessage !== "string"){
+      throw new TypeError("The argument must be a string");
+    } 
+    while (offsetValue2 > 26) {
+      offsetValue2 -= 26;
+    }
     let result = '';
     for (let index = 0; index < decipherMessage.length; index++) {
       const valueASCII = decipherMessage[index].charCodeAt();
       if (valueASCII >=65 && valueASCII<=90){
-        result += String.fromCharCode(((valueASCII-65 - offsetValue2 + 26)%26)+65);}  
-    } return result;  
+        result += String.fromCharCode(((valueASCII-65 - offsetValue2 + 26)%26)+65);
+      } 
+    } return result; 
   }
-}
+};
+
   
 
 
